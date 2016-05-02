ac2a6d95f4dbe53f7dda4a99c3d4e1e483cf8623
Juan <juan.trochez@hotmail.fr>
2016-05-02 15:21:52 +0200

    class player

--- BlindTest/Player.cs
+++ BlindTest/Player.cs
@@ -8,5 +8,43 @@ namespace BlindTest
 {
     class Player
     {
+        private String pseudo;
+        private int score;
+
+        public Player(String pseudo) {
+            this.pseudo = pseudo;
+            this.score = 0;
+        }
+
+        public string Pseudo
+        {
+            get
+            {
+                return pseudo;
+            }
+
+            set
+            {
+                pseudo = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+
+            set
+            {
+                score = value;
+            }
+        }
+
+        public void incrementScore() {
+            this.score++;
+        }
+
     }
 }
