861d2a7a19230096fec47e94c7ffd11761005fac
userinsta <lebrazideck@hotmail.fr>
2016-05-02 15:01:39 +0100

    Add class musique

--- BlindTest/Musique.cs
+++ BlindTest/Musique.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,57 @@ namespace BlindTest
 {
     class Musique
     {
+        private readonly string path = "C:\\Users\\userinsta\\Music\\MusicBlindTest";
+        private List<String> listMusic = new List<String>();
+        private int onPlay;
+
+        public Musique()
+        {
+            fillListMusic(ListMusic);
+        }
+
+        public void fillListMusic(List<String> listeMusic)
+        {
+            DirectoryInfo directory = new DirectoryInfo(Path);
+            FileInfo[] Files = directory.GetFiles("*.mp3");
+            foreach (FileInfo file in Files)
+            {
+                ListMusic.Add(file.Name);
+            }
+        }
+
+        public List<string> ListMusic
+        {
+            get
+            {
+                return listMusic;
+            }
+
+            set
+            {
+                listMusic = value;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+        }
+
+        public int OnPlay
+        {
+            get
+            {
+                return onPlay;
+            }
+
+            set
+            {
+                onPlay = value;
+            }
+        }
     }
 }
